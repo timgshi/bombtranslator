dca44e2e871a82db2b7f34275679e287109d9ee7
Chloe Yeung <cayeung@stanford.edu>
2012-03-07 17:49:58 -0800

    rends a file

--- src/bombtranslator.py
+++ src/bombtranslator.py
@@ -1,6 +1,19 @@
 import sys, re, time
 import collections
 
+class Translator:
+    def __init__(self):
+        self.vocab = list()
+    def buildVocab(self, text):
+        for word in text:
+            self.vocab.append(word)
+        print(len(self.vocab))
+
 #i know this is lame...so don't say anything GOD
 if __name__ == '__main__':
-    print("HI! I edited this file.")
+    filename = "lazarillo.txt"
+    f = open(filename, 'r')
+    textfile = f.readlines()
+    
+    translator = Translator()
+    translator.buildVocab(textfile)

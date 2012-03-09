4c5d37bd4314720a431630c30567cee06df508ed
Chloe Yeung <cayeung@stanford.edu>
2012-03-09 00:24:05 -0800

    added new files and tried to read them

--- src/bombtranslator.py
+++ src/bombtranslator.py
@@ -1,24 +1,35 @@
 import sys, re, time
-import collections
+import collections, codecs
 
 class Translator:
     def __init__(self):
         self.vocab = list()
+        self.spanishDict = {}
         self.spanishDict = self.buildDictionary()
     def buildVocab(self, text):
         for word in text:
             self.vocab.append(word)
-        print(len(self.vocab))
+        print(len(self.vochb))
     def buildDictionary(self):
-        print('hi')
+        dictname = 'dict.txt'
+        d = codecs.open(dictname, encoding='utf-8')
+        lines = d.readlines()
+        for line in lines:
+            line = line.strip('\n')
+            line = line.split('\t')
+            print(line)
+            self.spanishDict[line[0]] = line[1]
+            time.sleep(1)
         
             
         
 #i know this is lame...so don't say anything GOD
 if __name__ == '__main__':
-    filename = "lazarillo.txt"
-    f = open(filename, 'r')
-    textfile = f.readlines()
+    filename = "spanish_text.txt"
+    f = codecs.open(filename, 'r', "utf-8")
+    textfile = f.read()
+    print(textfile)
+    exit(1)
     
     translator = Translator()
     translator.buildVocab(textfile)

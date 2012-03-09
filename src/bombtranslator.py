b1839c357bb4a477ea59f57190d2884ba3f639fa
Chloe Yeung <cayeung@stanford.edu>
2012-03-09 01:32:41 -0800

    translated into english but missing puncuation

--- src/bombtranslator.py
+++ src/bombtranslator.py
@@ -5,26 +5,44 @@ class Translator:
     def __init__(self):
         self.vocab = list()
         self.spanishDict = {}
-        self.spanishDict = self.buildDictionary()
-    def buildVocab(self, text):
-        for word in text:
-            self.vocab.append(word)
-        print(len(self.vochb))
-    def buildDictionary(self):
+
+def buildDictionary():
+        dict ={}
         dictname = 'dict.txt'
         d = codecs.open(dictname, 'r', 'utf-8')
         for line in d:
             line = line.strip('\n')
             line = line.split()
-            self.spanishDict[line[0]] = line[1]
+            print(line[0])
+            dict[line[0].lower()] = line[1]
+        return dict
 
-        
+def translate(text, translator):        
+    filename = 'english.txt'
+    f = open(filename, 'w')
+    print('\n')
+    for line in text:
+        for word in line.split():
+            word = word.strip('.,')
+            if (word.lower() in translator.spanishDict):
+                f.write(translator.spanishDict[word.lower()] + " ")
+            else:
+                print('not found' + word)
+    f.close()
             
         
 #i know this is lame...so don't say anything GOD
 if __name__ == '__main__':
     filename = "spanish_text.txt"
     f = codecs.open(filename, 'r', 'utf-8')
-    textfile = f.read()
+    textfile = []
+    for line in f:
+        textfile.append(line)
     translator = Translator()
-    translator.buildVocab(textfile)
+    translator.spanishDict = buildDictionary()
+    f.close()
+
+    
+    translate(textfile, translator)
+    
+    
